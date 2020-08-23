3024feb01d7bf3b8dff75dd202a38dfcb645ccc5
TopgunJHW <67074169+TopgunJHW@users.noreply.github.com>
2020-08-23 11:06:02 +0200

    Create contract
    Create contract and test

---
+++ project/contracts/coinFlip.sol
@@ -0,0 +1,39 @@
+pragma solidity 0.5.12;
+
+contract coinFlip{
+
+  // uint public balance;
+
+  modifier costs(uint cost){
+      require(msg.value >= cost);
+      _;
+  }
+
+  event betResult(string result);
+  event contractFunded(uint amount);
+
+  function getBalance() public view returns(uint){
+    return address(this).balance;
+  }
+
+  function fundContract() public payable{
+    emit contractFunded(getBalance());
+  }
+
+  function random() public view returns(uint){
+    return now % 2;
+  }
+
+  function bet(uint answer) public payable costs(0.1 ether)returns(string memory){
+    string memory result = "You lose";
+
+    if(answer == random()){
+      msg.sender.transfer(msg.value * 2);
+      result = "You win";
+    };
+
+    emit betResult(result);
+    return Rr;
+  }
+
+}
---
+++ project/migrations/2_coinFlip_deploy.js
@@ -0,0 +1,13 @@
+const coinFlip = artifacts.require("coinFlip");
+
+module.exports = function(deployer, network, accounts) {
+  deployer.deploy(coinFlip).then(function(instance){
+    instance.fundContract({value: web3.utils.toWei("10", "ether")}).then(function(){
+      console.log("Initial balance set!")
+    }).catch(function(err){
+      console.log("Initial balance not set! Error: ");
+    });
+  }).catch(function(err){
+    console.log("Deploy failed! Error: " + err)
+  });
+};
