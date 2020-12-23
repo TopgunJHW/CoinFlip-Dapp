c283654f1dbdd3424df87325c359b41b17ab1a9d
TopgunJHW <topgun_jian_hao@hotmail.com>
2020-12-23 13:10:05 +0100

    Cleanup Dapp code
    Cleanup javascipt Dapp code.

--- dapp/main.js
+++ dapp/main.js
@@ -15,7 +15,6 @@ $(document).ready(function() {
       $("#get_allBets").click(getAllBets)
     });
 
-
     function bet(){
       var choice = document.getElementById("choice").textContent;
       var choiceNB = headOrTail(choice);
@@ -34,45 +33,18 @@ $(document).ready(function() {
         // alert("Is mined");
       })
       .then(function(){
-        // var from = contractInstance.options.from;
-        // console.log(from);
 
         contractInstance.methods.getQueryIDs(from).call().then(function(queryIDs){
-          console.log(queryIDs);
           var queryID = queryIDs[queryIDs.length-1];
-          console.log(queryID);
 
-          // contractInstance.once('betResult', {filter: {queryId: queryID}}, function (err, events){
-          //   console.log(events);
-          // contractInstance.once('testBlocknumber', function(err, events){
-          //   console.log(events);
-          // });
-
-          // contractInstance.methods.getBetInfo(queryID).call().then(function(betResult){
-          //   console.log(betResult);
-          //
-          //   var blockNumber = betResult.blockNumber;
-          //   var text2 = "You lost ";
-          //   var amount = betResult.betAmount;
-          //   if(betResult.guessNumber == betResult.randomNumber){
-          //     text2 = "You won ";
-          //     amount = String(parseInt(amount) * 2);
-          //   };
-          //
-          //   var choice = headOrTail(betResult.guessNumber);
-          //   var answer = headOrTail(betResult.randomNumber);
-          //
-          //   // $("#block_tbl").text("#1");
-          //   // $("#choice_tbl").text(choice);
-          //   // $("#answer_tbl").text(answer);
-          //   // $("#result_tbl").text(text2 + web3.utils.fromWei(amount, "ether") + " ether");
-          //   insert_Row(blockNumber, choice, answer, text2 + web3.utils.fromWei(amount, "ether") + " ether");
-
-          insertRow(queryID);
-
+          // console.log(queryIDs);
+          // console.log(queryID);
+          contractInstance.methods.getBetInfo(queryID).call().then(function(betResult){
+            insertRow(betResult);
           });
         });
-      };
+      });
+    };
 
     function getAllBets(){
 
@@ -82,37 +54,25 @@ $(document).ready(function() {
       var old_tbody = document.getElementById('table_result_body');
       old_tbody.parentNode.replaceChild(new_tbody, old_tbody);
 
-      // var newTableInfo = [];
-
       contractInstance.methods.getQueryIDs(from).call().then(function(queryIDs){
-        console.log(queryIDs)
-
-        // for (var i of queryIDs.length) {
-        //   queryID = queryIDs[i];
-        //   insertRow(queryID);
-        //   };
-
-        // for (var i = 0; i < queryIDs.length; i++) {
-        //   queryID = queryIDs[i];
-        //   insertRow(queryID);
-        // };
-          testt(0, queryIDs)
+        // console.log(queryIDs)
+        insertRowLoop(0, queryIDs)
         });
       };
 
-    function testt(i, queryIDs){
+    function insertRowLoop(i, queryIDs){
 
       if (i < queryIDs.length) {
         var queryID = queryIDs[i];
         contractInstance.methods.getBetInfo(queryID).call().then(function(betResult){
-          insertRow1(betResult);
+          insertRow(betResult);
           i++;
-          testt(i, queryIDs);
+          insertRowLoop(i, queryIDs);
         });
       };
     };
 
-    function insertRow1(betResult){
+    function insertRow(betResult){
       var blockNumber = betResult.blockNumber;
       var text2 = "You lost ";
       var amount = betResult.betAmount;
@@ -124,34 +84,9 @@ $(document).ready(function() {
         amount = String(parseInt(amount) * 2);
       };
 
+      // console.log(queryID);
+      // console.log(blockNumber);
       insertTableRow(blockNumber, choice, answer, text2 + web3.utils.fromWei(amount, "ether") + " ether");
-    };
-
-    function insertRow(queryID){
-      console.log(queryID);
-      contractInstance.methods.getBetInfo(queryID).call().then(function(betResult){
-        // console.log(betResult);
-
-        var blockNumber = betResult.blockNumber;
-        var text2 = "You lost ";
-        var amount = betResult.betAmount;
-        if(betResult.guessNumber == betResult.randomNumber){
-          text2 = "You won ";
-          amount = String(parseInt(amount) * 2);
-        };
-
-        var choice = headOrTail(betResult.guessNumber);
-        var answer = headOrTail(betResult.randomNumber);
-
-        // $("#block_tbl").text("#1");
-        // $("#choice_tbl").text(choice);
-        // $("#answer_tbl").text(answer);
-        // $("#result_tbl").text(text2 + web3.utils.fromWei(amount, "ether") + " ether");
-        // newTableInfo[i] = blockNumber;
-        console.log(queryID)
-        console.log(blockNumber)
-        insertTableRow(blockNumber, choice, answer, text2 + web3.utils.fromWei(amount, "ether") + " ether");
-      });
     };
 
     function insertTableRow(inputC0, inputC1, inputC2, inputC3){
